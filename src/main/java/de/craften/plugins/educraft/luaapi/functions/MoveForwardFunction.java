cc9cae41e368e6834190c0316a9425b6bcba6ecd
leMaik <an@maik.tk>
2015-12-29 23:58:02 +0100

    Check for entities ahead before moving forward.

--- src/main/java/de/craften/plugins/educraft/luaapi/functions/MoveForwardFunction.java
+++ src/main/java/de/craften/plugins/educraft/luaapi/functions/MoveForwardFunction.java
@@ -3,8 +3,11 @@ package de.craften.plugins.educraft.luaapi.functions;
 import de.craften.plugins.educraft.luaapi.EduCraftApiFunction;
 import org.bukkit.Location;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Entity;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
+
+import java.util.Collection;
 
 /**
  * Lua API function to move one block forward.
@@ -13,9 +16,11 @@ public class MoveForwardFunction extends EduCraftApiFunction {
     @Override
     public Varargs execute(Varargs varargs) {
         Location targetLocation = getApi().getStationaryBehavior().getLocation().clone().add(getApi().getDirection());
+        Collection<Entity> entities = targetLocation.getWorld().getNearbyEntities(targetLocation.getBlock().getLocation().add(0.5, 0.5, 0.5), 0.5, 0.5, 0.5);
 
         if (!targetLocation.getBlock().getType().isSolid()
-                && !targetLocation.getBlock().getRelative(BlockFace.UP).getType().isSolid()) {
+                && !targetLocation.getBlock().getRelative(BlockFace.UP).getType().isSolid()
+                && entities.isEmpty()) {
             getApi().getStationaryBehavior().setLocation(targetLocation);
         }
 
