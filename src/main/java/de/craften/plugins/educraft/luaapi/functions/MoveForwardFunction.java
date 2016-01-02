910474568881ed229c3fdfddbafb8bf2a2740546
leMaik <an@maik.tk>
2016-01-02 21:30:03 +0100

    Make entity fall if there is no solid block below it.

--- src/main/java/de/craften/plugins/educraft/luaapi/functions/MoveForwardFunction.java
+++ src/main/java/de/craften/plugins/educraft/luaapi/functions/MoveForwardFunction.java
@@ -4,11 +4,8 @@ import de.craften.plugins.educraft.luaapi.EduCraftApiFunction;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.entity.Entity;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
-
-import java.util.Collection;
 
 /**
  * Lua API function to move one block forward.
@@ -18,7 +15,6 @@ public class MoveForwardFunction extends EduCraftApiFunction {
     public Varargs execute(Varargs varargs) {
         Block blockAhead = getApi().getBlockAhead();
         Location locationAhead = blockAhead.getLocation();
-        Collection<Entity> entities = getApi().getEntitiesAhead();
 
         if (!blockAhead.getType().isSolid()
                 && !blockAhead.getRelative(BlockFace.UP).getType().isSolid()
@@ -26,6 +22,11 @@ public class MoveForwardFunction extends EduCraftApiFunction {
                 && getApi().getEnvironment().contains(locationAhead)) {
             getApi().moveTo(locationAhead, false);
         }
+
+        while (!blockAhead.getRelative(BlockFace.DOWN).getType().isSolid() && blockAhead.getY() > 0) {
+            blockAhead = blockAhead.getRelative(BlockFace.DOWN);
+        }
+        getApi().moveTo(blockAhead.getLocation(), false);
 
         return LuaValue.NIL;
     }
